eba9c7e9b10fa2205451bdff759cb9b59493bbb6
gabypizzutti <gabrielapizzutti@gmail.com>
2022-04-04 12:45:39 -0300

    se realizan modificaciones para la entrega

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,8 +2,8 @@ import ItemCount from "./ItemCount";
 
 const ItemListContainer = ({greeting}) => {
 
-    const onAdd = (item) => {
-        alert('${item} agregados a tu carrito');
+    const onAdd = (items) => {
+        alert(`${items} items agregados a tu carrito`);
     }
 
     return (
