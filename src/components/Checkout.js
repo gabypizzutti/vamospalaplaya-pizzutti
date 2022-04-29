8782c375b59104617b856721e3ceca5dba4667a5
gabypizzutti <gabrielapizzutti@gmail.com>
2022-04-29 18:07:38 -0300

    limpio para traer proyecto que anda

--- src/components/Checkout.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-const Checkout = () => {
-    return(
-       <Link to="/Cart"><button className="btnCheckout">Checkout</button></Link>
-    )
-}
-
-export default Checkout;
